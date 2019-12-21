a04f6e02c3e592d9058ddc44a6bf2a9c24173a1d
Daniel Iglesias <diglesias78@gmail.com>
2019-12-21 09:47:36 +0000

    Progress on power input, partially reading from file

--- vitagui/widget_input.py
+++ vitagui/widget_input.py
@@ -11,7 +11,7 @@ from PyQt5.QtWidgets import (QApplication, QComboBox, QDateTimeEdit,
         QDial, QWidget, QGridLayout, QGroupBox, QHBoxLayout, QLabel, QLineEdit,
         QProgressBar, QPushButton, QRadioButton, QScrollBar, QSizePolicy,
         QSlider, QSpinBox, QDoubleSpinBox, QTableWidget, QTabWidget, QTextEdit,
-        QVBoxLayout)
+        QVBoxLayout, QCheckBox)
 
 
 class WidgetInput(QWidget):
@@ -21,15 +21,15 @@ class WidgetInput(QWidget):
         self.createTopGroup()
         self.createTopLeftGroupBox()
         self.createTopRightGroupBox()
-        self.createBottomLeftTabWidget()
+        self.createtopLeftTabWidget()
         self.createBottomRightGroupBox()
         self.createProgressBar()
 
         mainLayout = QGridLayout()
         mainLayout.addLayout(self.topLayout, 0, 0, 1, 2)
-        mainLayout.addWidget(self.topLeftGroupBox, 1, 0)
+        mainLayout.addWidget(self.topLeftTabWidget, 1, 0)
         mainLayout.addWidget(self.topRightGroupBox, 1, 1)
-        mainLayout.addWidget(self.bottomLeftTabWidget, 2, 0)
+        mainLayout.addWidget(self.bottomLeftGroupBox, 2, 0)
         mainLayout.addWidget(self.bottomRightGroupBox, 2, 1)
         mainLayout.addWidget(self.progressBar, 3, 0, 1, 2)
         mainLayout.setRowStretch(1, 1)
@@ -57,6 +57,15 @@ class WidgetInput(QWidget):
     def setData(self, inputData):
         self.data = inputData
         self.textEditData.setPlainText(str(self.data))
+        if 'heating' in self.data['plasma-settings']:
+            aux_power = 0.0
+            if 'NBI-power' in self.data['plasma-settings']['heating'] : 
+                aux_power += self.data['plasma-settings']['heating']['NBI-power']
+            if 'rf-power' in self.data['plasma-settings']['heating'] : 
+                aux_power += self.data['plasma-settings']['heating']['rf-power']
+            if 'Ohmic-power' in self.data['plasma-settings']['heating'] : 
+                aux_power += self.data['plasma-settings']['heating']['Ohmic-power']
+            self.pauxDSB.setValue(aux_power)
 
 
     def changeWorkflow(self, styleName):
@@ -93,7 +102,9 @@ class WidgetInput(QWidget):
         self.progressBar.setValue(curVal + (maxVal - curVal) / 100)
 
     def createTopGroup(self):
-        self.idNumber = QLabel("1234567890")
+        self.idNumber = QSpinBox()
+        self.idNumber.setRange(0,9999)
+        self.idNumber.setValue(1234)
         self.idLabel = QLabel("&ID:")
         self.idLabel.setBuddy(self.idNumber)
 
@@ -119,7 +130,153 @@ class WidgetInput(QWidget):
         self.topLayout.addWidget(self.machineComboBox)
 
     def createTopLeftGroupBox(self):
-        self.topLeftGroupBox = QGroupBox("Equilibrium")
+        self.topLeftTabWidget = QTabWidget()
+        # self.topLeftTabWidget.setSizePolicy(QSizePolicy.Preferred,
+                # QSizePolicy.Ignored)
+
+        tab1 = QWidget()
+        # tableWidget = QTableWidget(10, 10)
+        tab1hbox = QGridLayout()
+        tab1hbox.setContentsMargins(5, 5, 5, 5)
+        # tab1hbox.addWidget(tableWidget)
+
+        isotopeLabel = QLabel("Isotopes")
+        self.hydrogenButton = QRadioButton("H/D")
+        self.DTButton = QRadioButton("D-T")
+        self.hydrogenButton.setChecked(True)
+
+
+        bottomLeftGroupBoxPower = QGroupBox("SOL Power")
+        # self.lambdaLabel = QLabel("&Lambda_q (mm):")
+        self.pauxBox = QCheckBox("Paux (MW)")
+        self.pauxDSB = QDoubleSpinBox()
+        self.pauxBox.setChecked(True)
+        self.pradBox = QCheckBox("Prad:")
+        self.pradfractionButton = QRadioButton("Ratio (%)")
+        self.pradfractionDSB = QDoubleSpinBox()
+        self.pradvalueButton = QRadioButton("Value (MW)")
+        self.pradvalueDSB = QDoubleSpinBox()
+        self.pradBox.setChecked(True)
+        self.pradvalueButton.setChecked(True)
+
+
+        layout = QGridLayout()
+        # layout.addWidget(self.lambdaLabel,       0, 0, 1, 2)
+        layout.addWidget(self.pauxBox,            1, 0)
+        layout.addWidget(self.pauxDSB,            1, 1)
+        layout.addWidget(self.pradBox,            2, 0)
+        layout.addWidget(self.pradfractionButton, 3, 0)
+        layout.addWidget(self.pradfractionDSB,    3, 1)
+        layout.addWidget(self.pradvalueButton,    4, 0)
+        layout.addWidget(self.pradvalueDSB,       4, 1)
+        bottomLeftGroupBoxPower.setLayout(layout)
+
+        bottomLeftGroupBoxOther = QGroupBox("Target fractions")
+        # self.lambdaLabel = QLabel("&Lambda_q (mm):")
+        self.shareButton = QRadioButton("Manual")
+        self.drsepButton = QRadioButton("DRsep")
+        self.shareButton.setChecked(True)
+        self.uhfsLabel = QLabel("Up HFS")
+        self.uhfsDSB = QDoubleSpinBox()
+        self.uhfsLabel.setBuddy(self.uhfsDSB)
+        self.ulfsLabel = QLabel("Up LFS")
+        self.ulfsDSB = QDoubleSpinBox()
+        self.ulfsLabel.setBuddy(self.ulfsDSB)
+        self.dhfsLabel = QLabel("Down HFS")
+        self.dhfsDSB = QDoubleSpinBox()
+        self.dhfsLabel.setBuddy(self.dhfsDSB)
+        self.dlfsLabel = QLabel("down HFS")
+        self.dlfsDSB = QDoubleSpinBox()
+        self.dlfsLabel.setBuddy(self.dlfsDSB)
+        
+        layout = QGridLayout()
+        # layout.addWidget(self.lambdaLabel,       0, 0, 1, 2)
+        layout.addWidget(self.shareButton,      1, 0)
+        layout.addWidget(self.drsepButton,      1, 1)
+        layout.addWidget(self.uhfsLabel,        2, 0)
+        layout.addWidget(self.uhfsDSB,          2, 1)
+        layout.addWidget(self.ulfsLabel,        3, 0)
+        layout.addWidget(self.ulfsDSB,          3, 1)
+        layout.addWidget(self.dhfsLabel,        4, 0)
+        layout.addWidget(self.dhfsDSB,          4, 1)
+        layout.addWidget(self.dlfsLabel,        5, 0)
+        layout.addWidget(self.dlfsDSB,          5, 1)
+        bottomLeftGroupBoxOther.setLayout(layout)
+
+        tab1hbox.addWidget(isotopeLabel,   1, 0)
+        tab1hbox.addWidget(self.hydrogenButton, 1, 1)
+        tab1hbox.addWidget(self.DTButton, 1, 2)
+        tab1hbox.addWidget(bottomLeftGroupBoxPower, 2, 0, 1, 3)
+        tab1hbox.addWidget(bottomLeftGroupBoxOther, 3, 0, 1, 3)
+        tab1.setLayout(tab1hbox)
+
+        tab2 = QWidget()
+        self.textEditData = QTextEdit()
+
+        tab2hbox = QHBoxLayout()
+        tab2hbox.setContentsMargins(5, 5, 5, 5)
+        tab2hbox.addWidget(self.textEditData)
+        tab2.setLayout(tab2hbox)
+
+        tab3 = QWidget()
+        self.textEditResources = QTextEdit()
+
+        tab3hbox = QHBoxLayout()
+        tab3hbox.setContentsMargins(5, 5, 5, 5)
+        tab3hbox.addWidget(self.textEditResources)
+        tab3.setLayout(tab3hbox)
+
+        self.topLeftTabWidget.addTab(tab1, "Parameters")
+        self.topLeftTabWidget.addTab(tab2, "Input data")
+        self.topLeftTabWidget.addTab(tab3, "Local resources")
+
+    def createTopRightGroupBox(self):
+        self.topRightGroupBox = QGroupBox("SOL parameters")
+
+        topRightGroupBoxLambda = QGroupBox("Lambda_q (mm)")
+        # self.lambdaLabel = QLabel("&Lambda_q (mm):")
+        self.mmButton = QRadioButton("MM 2011")
+        self.mmDSB = QDoubleSpinBox()
+        self.eichButton = QRadioButton("Eich #15")
+        self.eichDSB = QDoubleSpinBox()
+        self.lambdaManualButton = QRadioButton("Manual")
+        self.lambdaManualDSB = QDoubleSpinBox()
+        self.lambdaManualButton.setChecked(True)
+
+        layout = QGridLayout()
+        # layout.addWidget(self.lambdaLabel,       0, 0, 1, 2)
+        layout.addWidget(self.mmButton,          1, 0)
+        layout.addWidget(self.mmDSB,             1, 1)
+        layout.addWidget(self.eichButton,        2, 0)
+        layout.addWidget(self.eichDSB,           2, 1)
+        layout.addWidget(self.lambdaManualButton,3, 0)
+        layout.addWidget(self.lambdaManualDSB,   3, 1)
+        topRightGroupBoxLambda.setLayout(layout)
+
+        topRightGroupBoxS = QGroupBox("S (mm)")
+        # self.sLabel = QLabel("&S (mm):")
+        self.sFractionButton = QRadioButton("Fraction")
+        self.sFractionDSB = QDoubleSpinBox()
+        self.sManualButton = QRadioButton("Manual")
+        self.sManualDSB = QDoubleSpinBox()
+        self.sManualButton.setChecked(True)
+
+        layout = QGridLayout()
+        # layout.addWidget(self.sLabel,            4, 0, 1, 2)
+        layout.addWidget(self.sFractionButton,    5, 0)
+        layout.addWidget(self.sFractionDSB,       5, 1)
+        layout.addWidget(self.sManualButton,     6, 0)
+        layout.addWidget(self.sManualDSB,        6, 1)
+        topRightGroupBoxS.setLayout(layout)
+
+        layout = QVBoxLayout()
+        layout.addWidget(topRightGroupBoxLambda)
+        layout.addWidget(topRightGroupBoxS)
+
+        self.topRightGroupBox.setLayout(layout)
+
+    def createtopLeftTabWidget(self):
+        self.bottomLeftGroupBox = QGroupBox("Equilibrium")
 
         self.equilibriumButton1 = QRadioButton("Static")
         self.equilibriumButton2 = QRadioButton("Sweeping")
@@ -154,105 +311,7 @@ class WidgetInput(QWidget):
         self.sweepFrequencyLabel.hide()
         self.sweepFrequency.hide()
         layout.addStretch(1)
-        self.topLeftGroupBox.setLayout(layout)    
-
-    def createTopRightGroupBox(self):
-        self.topRightGroupBox = QGroupBox("SOL parameters")
-
-        topRightGroupBoxLambda = QGroupBox("Lambda_q (mm)")
-        # self.lambdaLabel = QLabel("&Lambda_q (mm):")
-        self.mmButton = QRadioButton("MM 2011")
-        self.mmDSB = QDoubleSpinBox()
-        self.eichButton = QRadioButton("Eich #15")
-        self.eichDSB = QDoubleSpinBox()
-        self.lambdaManualButton = QRadioButton("Manual")
-        self.lambdaManualDSB = QDoubleSpinBox()
-        self.lambdaManualButton.setChecked(True)
-
-        layout = QGridLayout()
-        # layout.addWidget(self.lambdaLabel,       0, 0, 1, 2)
-        layout.addWidget(self.mmButton,          1, 0)
-        layout.addWidget(self.mmDSB,             1, 1)
-        layout.addWidget(self.eichButton,        2, 0)
-        layout.addWidget(self.eichDSB,           2, 1)
-        layout.addWidget(self.lambdaManualButton,3, 0)
-        layout.addWidget(self.lambdaManualDSB,   3, 1)
-        topRightGroupBoxLambda.setLayout(layout)
-
-        topRightGroupBoxS = QGroupBox("S (mm)")
-        # self.sLabel = QLabel("&S (mm):")
-        self.makowskiButton = QRadioButton("Eich #15")
-        self.makowskiDSB = QDoubleSpinBox()
-        self.sManualButton = QRadioButton("Manual")
-        self.sManualDSB = QDoubleSpinBox()
-
-        layout = QGridLayout()
-        # layout.addWidget(self.sLabel,            4, 0, 1, 2)
-        layout.addWidget(self.makowskiButton,    5, 0)
-        layout.addWidget(self.makowskiDSB,       5, 1)
-        layout.addWidget(self.sManualButton,     6, 0)
-        layout.addWidget(self.sManualDSB,        6, 1)
-        topRightGroupBoxS.setLayout(layout)
-
-        layout = QVBoxLayout()
-        layout.addWidget(topRightGroupBoxLambda)
-        layout.addWidget(topRightGroupBoxS)
-
-        self.topRightGroupBox.setLayout(layout)
-
-    def createBottomLeftTabWidget(self):
-        self.bottomLeftTabWidget = QTabWidget()
-        # self.bottomLeftTabWidget.setSizePolicy(QSizePolicy.Preferred,
-                # QSizePolicy.Ignored)
-
-        tab1 = QWidget()
-        # tableWidget = QTableWidget(10, 10)
-
-        bottomLeftGroupBoxPower = QGroupBox("Lambda_q (mm)")
-        # self.lambdaLabel = QLabel("&Lambda_q (mm):")
-        self.powerButton = QRadioButton("MM 2011")
-        self.powerDSB = QDoubleSpinBox()
-        self.eichButton = QRadioButton("Eich #15")
-        self.eichDSB = QDoubleSpinBox()
-        self.lambdaManualButton = QRadioButton("Manual")
-        self.lambdaManualDSB = QDoubleSpinBox()
-        self.lambdaManualButton.setChecked(True)
-
-        layout = QGridLayout()
-        # layout.addWidget(self.lambdaLabel,       0, 0, 1, 2)
-        layout.addWidget(self.mmButton,          1, 0)
-        layout.addWidget(self.mmDSB,             1, 1)
-        layout.addWidget(self.eichButton,        2, 0)
-        layout.addWidget(self.eichDSB,           2, 1)
-        layout.addWidget(self.lambdaManualButton,3, 0)
-        layout.addWidget(self.lambdaManualDSB,   3, 1)
-        bottomLeftGroupBoxPower.setLayout(layout)
-
-        tab1hbox = QHBoxLayout()
-        tab1hbox.setContentsMargins(5, 5, 5, 5)
-        # tab1hbox.addWidget(tableWidget)
-        tab1hbox.addWidget(bottomLeftGroupBoxPower)
-        tab1.setLayout(tab1hbox)
-
-        tab2 = QWidget()
-        self.textEditData = QTextEdit()
-
-        tab2hbox = QHBoxLayout()
-        tab2hbox.setContentsMargins(5, 5, 5, 5)
-        tab2hbox.addWidget(self.textEditData)
-        tab2.setLayout(tab2hbox)
-
-        tab3 = QWidget()
-        self.textEditResources = QTextEdit()
-
-        tab3hbox = QHBoxLayout()
-        tab3hbox.setContentsMargins(5, 5, 5, 5)
-        tab3hbox.addWidget(self.textEditResources)
-        tab3.setLayout(tab3hbox)
-
-        self.bottomLeftTabWidget.addTab(tab1, "&Power")
-        self.bottomLeftTabWidget.addTab(tab2, "Input data")
-        self.bottomLeftTabWidget.addTab(tab3, "Local resources")
+        self.bottomLeftGroupBox.setLayout(layout)    
 
     def createBottomRightGroupBox(self):
         self.plotPushButton = QPushButton("Default Push Button")
